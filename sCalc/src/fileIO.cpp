f3d1636b4577bd7d466451116f2dd9a3cc5f4ec6
Someone <sangitabiswas0606@gmail.com>
2021-08-04 00:32:05 +0530

    FINALLY! Got it to work after 2 days of hardwork(actually uhhh I was lazy)

---
+++ sCalc/src/fileIO.cpp
@@ -0,0 +1,36 @@
+#include "fileIO.h"
+
+void writeFile(std::string& fName, bool choice)
+{
+	std::ofstream file;
+	file.open(fName, std::ios_base::out);
+	file << choice;
+}
+
+bool readFile(std::string& fName)
+{
+	bool stC = false;
+	std::string validation = "";
+
+	std::ifstream file;
+	file.open(fName);
+	if (!file)
+	{
+		std::ofstream f;
+		f.open(fName, std::ios_base::out);
+		file >> stC;
+		f.close();
+	}
+	
+	file >> validation;
+	if (validation == "")
+		return false;
+	if (validation.length() == 1 && validation.at(0) >= '0' && validation.at(0) <= '9')
+		return (validation.at(0) - '0' == 0 ? false : true);
+	else {
+		std::cout << "Error reading setting file." << std::endl;
+		return false;
+	}
+	file.close();
+	return false;
+}
