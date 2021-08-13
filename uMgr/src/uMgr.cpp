300c8ef034043e92aa67fb8e7be9e58978476789
Someone <sangitabiswas0606@gmail.com>
2021-08-13 21:29:57 +0530

    Implemented folderio? yeah whatever... Some basic folder management that it'll require. There's a lot more to be done.

--- uMgr/src/uMgr.cpp
+++ uMgr/src/uMgr.cpp
@@ -1,8 +1,54 @@
-#include <iostream>
-
-#define pr(wtf) std::cout << wtf
-#define prl(wtf) std::cout << wtf << std::endl
+#include "fileIO.h"
 
 int main() {
-	prl("Hewwo World");
+	str name = folderN + fsep + "Settings.baka"; // :) use mah other product (scalc) to know the origin of the file extention
+	str validation = "";
+
+	fs::create_directory(folderN);
+	fstream setFile;
+
+	{
+		ofstream f;
+		if (!fs::exists(name))
+		{
+			f.open(name);
+			f << 0;
+			f.close();
+		}
+		
+		if (!fs::exists((folderN + fsep + "LastLog.baka"))) // :)
+		{
+			f.open(folderN + fsep + "LastLog.baka");
+			f.close();
+		}
+	}
+
+	bool mkgenre = 0;
+	setFile.open(name);
+
+	while (getline(setFile, validation))
+	{
+		if (validation == "")
+			continue;
+		else if (isspace(validation))
+			continue;
+		else if (validation.length() == 1 && validation.at(0) >= '0' && validation.at(0) <= '9')
+		{
+			mkgenre = (validation.at(0) - '0');
+		}
+		else
+		{
+			prl("\nError reading setting file\n");
+			return false;
+		}
+	}
+
+	// creating some predefined genres cuz... fu, look at my README.md
+	if (mkgenre == 0)
+	{
+		fs::create_directory(folderN + fsep + "Anime");
+		fs::create_directory(folderN + fsep + "Manga");
+		fs::create_directory(folderN + fsep + "Movies");
+	}
+	setFile.close();
 }
