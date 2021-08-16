f0ec475dafea068c7ec8b36a683afccd77064460
Someone <sangitabiswas0606@gmail.com>
2021-08-16 17:40:08 +0530

    Basic read file test

--- uMgr/src/uMgr.cpp
+++ uMgr/src/uMgr.cpp
@@ -1,54 +1,51 @@
 #include "fileIO.h"
 
 int main() {
-	str name = folderN + fsep + "Settings.baka"; // :) use mah other product (scalc) to know the origin of the file extention
-	str validation = "";
-
-	fs::create_directory(folderN);
-	fstream setFile;
-
-	{
-		ofstream f;
-		if (!fs::exists(name))
-		{
-			f.open(name);
-			f << 0;
-			f.close();
-		}
-		
-		if (!fs::exists((folderN + fsep + "LastLog.baka"))) // :)
-		{
-			f.open(folderN + fsep + "LastLog.baka");
-			f.close();
-		}
-	}
-
-	bool mkgenre = 0;
-	setFile.open(name);
-
-	while (getline(setFile, validation))
-	{
-		if (validation == "")
-			continue;
-		else if (isspace(validation))
-			continue;
-		else if (validation.length() == 1 && validation.at(0) >= '0' && validation.at(0) <= '9')
-		{
-			mkgenre = (validation.at(0) - '0');
-		}
-		else
-		{
-			prl("\nError reading setting file\n");
-			return false;
-		}
-	}
-
-	// creating some predefined genres cuz... fu, look at my README.md
-	if (mkgenre == 0)
-	{
-		fs::create_directory(folderN + fsep + "Anime");
-		fs::create_directory(folderN + fsep + "Manga");
-		fs::create_directory(folderN + fsep + "Movies");
-	}
-	setFile.close();
+    fs::create_directory(folderN);
+    {
+        str paf = folderN + fsep + "Settings.baka";
+        if (!fs::exists((paf))) {
+            ofstream f;
+            f.open(paf);
+            f << (char)0;
+            f.close();
+        }
+        // idk if this works but does it improve the memory management (but destroying[or whatever it's called] the string var)
+        {
+            str pa = folderN + fsep + "LastLog.baka";
+            if (!fs::exists(pa)) {
+                ofstream file;
+                file.open(pa);
+                file << (char)1 << endl;
+                file.close();
+            }
+        }
+        {
+            bool createPredefinedGenre = 1;
+            str validation, pa = folderN + fsep;
+            char c;
+            ifstream file;
+            file.open(paf);
+            while (getline(file, validation)) {
+                c = validation.at(0);
+                if (validation == "")
+                    createPredefinedGenre = 1;
+                else if ((c >= '0' || c <= '9') && validation.length() == 1)
+                    createPredefinedGenre = (c - '0');
+                else {
+                    prl("Error reading settings file");
+                    break;
+                }
+            }
+            if (createPredefinedGenre) {
+                fs::create_directory((pa + "Anime"));
+                fs::create_directory((pa + "Manga"));
+                fs::create_directory((pa + "Movies"));
+            }
+        }
+    }
+    ifstream file;
+    file.open((folderN + fsep + "LastLog.baka"));
+    readFile(file, Others, 0, (folderN + fsep + "LastLog.baka"));
+    return 0;
 }
