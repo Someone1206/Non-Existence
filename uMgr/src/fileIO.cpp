62e633c38d31ad8afc5527dfaad22aa3e51df234
Someone <sangitabiswas0606@gmail.com>
2021-08-16 17:54:55 +0530

    Read file --> Others Added ways of reading files(entries) marked as Others

--- uMgr/src/fileIO.cpp
+++ uMgr/src/fileIO.cpp
@@ -1,12 +1,120 @@
 #include "fileIO.h"
 
-bool isspace(str& s)
-{
-	const char* c = s.c_str(); // i will probable never use multibyte char for this
-	for (int i = 0; i < s.length(); i++)
-	{
-		if (isspace(*c++))
-			return true;
-	}
-	return false;
+bool isspace(str& string1) {
+    const char* c = string1.c_str();
+
+    for (int i = 0; i < string1.length(); ++i)
+        if (isspace(*c++))
+            return false;
+    return true;
+}
+
+/* ******************************************************************************
+ *
+ * How the file will be read (this is bound to change):
+ * <!-- Others:  distinguished by file names-->
+ * <name>
+ * (char)0
+ * <date>
+ * <time>
+ * <details> ...optional
+ * (char)0
+ * <date>
+ * <time>
+ * <details> ...optional
+ *
+ * <!-- Anime:  distinguished by file names-->
+ * <name>
+ * (char)0
+ * <date>
+ * <time>
+ * <season>  ...optional
+ * <episode> ...optional
+ * <details> ...optional
+ * (char)0
+ *
+ * <!-- Manga:  distinguished by file names-->
+ * <name>
+ * (char)0
+ * <date>
+ * <time>
+ * <chapter> ...optional
+ * <page>    ...optional
+ * <details> ...optional
+ * (char)0
+ *
+ * <!-- Movies:  distinguished by file names-->
+ * <name>
+ * (char)0
+ * <date>
+ * <time>
+ * <part>    ...optional
+ * <details> ...optional
+ * (char)0
+ *
+ * <!-- Settings.baka -->
+ * <choice>
+ *
+ *
+ * <!-- LastLogs.baka -->
+ * <genre>
+ * <name>
+ * <details>
+ * (char)0
+ * ...
+ * ...
+ * ...
+ * ...shows last 10 logs
+ * ******************************************************************************
+ */
+
+void readFile(ifstream& file, Options options, int history, str paf) {
+
+    if (!file.is_open()) {
+        file.open(paf);
+        if (!fs::exists(paf)) {
+            prl("Error Opening file");
+            return;
+        }
+    }
+
+    str line = "";
+    int counter = 0;
+
+    // TODO: 
+
+    history = (bool)history * history + ((!(bool)history) * -1) + (((bool)history) * 1); // problem here
+    // if history is 0, print all -> -1, else print the number of logs from first and add 1 to it
+    getline(file, line);
+    prl("Name:");
+    prl(line);
+
+    switch (options) {
+    case Others:
+        
+        while (getline(file, line) && ((bool)history)) {
+            
+            if (line.find((char)1) != std::string::npos) {
+                counter = 0;
+                history--;
+                continue;
+            }
+            
+            switch (counter)
+            {
+            case 0:
+                prl("Date:" << '\n' << line << '\n');
+                break;
+            case 1:
+                prl("Time:" << '\n' << line);
+                break;
+            case 2:
+                prl("Details:");
+            default:
+                prl(line);
+            }
+            counter++;
+        }
+        break;
+    }
 }
