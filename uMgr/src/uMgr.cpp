132bed95e24fd535830e1dab22b7b3d3ee99f0b6
Someone <sangitabiswas0606@gmail.com>
2021-08-17 17:43:34 +0530

    Ehh Idk Implemented the read tracker file fn for settings.baka also tested the others (not for errors).

--- uMgr/src/uMgr.cpp
+++ uMgr/src/uMgr.cpp
@@ -7,11 +7,12 @@ int main() {
         if (!fs::exists((paf))) {
             ofstream f;
             f.open(paf);
+            f << 0;
             f.close();
         }
         // idk if this works but does it improve the memory management (by destroying[or whatever it's called] the string var)
         {
-            str pa = folderN + fsep + "LastLog.baka";
+            str pa = folderN + fsep + "LastLogs.baka";
             if (!fs::exists(pa)) {
                 ofstream file;
                 file.open(pa);
@@ -21,21 +22,12 @@ int main() {
         }
         {
             bool createPredefinedGenre = 1;
-            str validation, pa = folderN + fsep;
-            char c;
+            str pa = folderN + fsep;
             ifstream file;
             file.open(paf);
-            while (getline(file, validation)) {
-                c = validation.at(0);
-                if (validation == "")
-                    createPredefinedGenre = 1;
-                else if ((c >= '0' || c <= '9') && validation.length() == 1)
-                    createPredefinedGenre = (c - '0');
-                else {
-                    prl("Error reading settings file");
-                    break;
-                }
-            }
+
+            readTrackerFile(file, createPredefinedGenre);
+
             if (createPredefinedGenre) {
                 fs::create_directory((pa + "Anime"));
                 fs::create_directory((pa + "Manga"));
@@ -44,9 +36,7 @@ int main() {
         }
     }
     ifstream file;
-    file.open((folderN + fsep + "Anime" + fsep + "Date A Live.log"));
-    readFile(file, Anime, 2); // Anime, Manga, Movies and Others work fabulously! Okay, I didn't take any errors into account while reading the file
-    // looking at the formatting of the file, it seems if you play with the files nad mess up, you screwed yourself. 
-    // cuz there's no way to detect errors according to the syntax but will probably come soon, I guess (that's a big guess).
+    file.open((folderN + fsep + "LastLogs.baka"));
+    readTrackerFile(file, LastLogs, 2);
     return 0;
 }
