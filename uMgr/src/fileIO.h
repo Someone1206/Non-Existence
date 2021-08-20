d74083b068d1fdbda9f24c07beb164d683bb7182
Someone <sangitabiswas0606@gmail.com>
2021-08-20 19:29:41 +0530

    Write file fns Write file fns for settings, others and Anime added and overloaded.

--- uMgr/src/fileIO.h
+++ uMgr/src/fileIO.h
@@ -20,13 +20,11 @@ namespace fs = std::filesystem;
 
 static str folderN = "uMgrData";
 
-enum Options {
+enum ReadOptions {
     Anime, Manga, Movies,
 
     Others
 };
-
-// why 2 different enums? ytf not?
 
 enum TrackerFileOptions
 {
@@ -36,9 +34,24 @@ enum TrackerFileOptions
 
 bool isspace(str& string1);
 
-void readFile(ifstream& file, Options options, int history = 0, str paf = "", bool isLLog = 0);
+
+
+// read from files
+//
+void readFile(ifstream& file, ReadOptions options, int history = 0, bool isLLog = 0);
 
 void readTrackerFile(ifstream& file, TrackerFileOptions tfo, int history = 0);
 // history shows no. of history for LastLogs.baka, if > 10 then it shutsdown silently
 
 void readTrackerFile(ifstream& file, bool& choice); // only for settings.baka
+
+
+
+// write to files
+
+void writeFile(str paf, str& details, str& date, str& time, str name = "");
+// write file for others
+
+void writeFile(str paf, str& season, str& episode, str& details, str& date, str& time, str name = "");
+// write file for Anime
+void writeFile(str paf, bool choice);
