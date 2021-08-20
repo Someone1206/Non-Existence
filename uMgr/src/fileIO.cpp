80af8abcd0ec41acc725645190f7c8176b8d5028
Someone <sangitabiswas0606@gmail.com>
2021-08-20 19:35:33 +0530

    Write file fns Write file fns for others and settings.baka

--- uMgr/src/fileIO.cpp
+++ uMgr/src/fileIO.cpp
@@ -74,32 +74,27 @@ bool isspace(str& string1) {
  *
  *
  * <!-- GenIndexer.baka (G_IndexerAndData)-->
- * 
+ *
  * <!!-- Names to display for the Genre --!>
- * <-- some OS may not support Unicode in its filesystem (which OS is so damn outdated?) 
+ * <-- some OS may not support Unicode in its filesystem (which OS is so damn outdated?)
  * or may show errors or stupid behaviours for some characters (I'm not trying to say that
  * English is great but I've seen some linux distros do that, Windows doesn't show any problems
  * its good -->
  *
  * <!-- EntryIndexerEng.baka -->
- * 
+ *
  * <!!-- Names to display for the Entry --!>
- * 
+ *
  * ******************************************************************************
  */
 
-void readFile(ifstream& file, Options options, int history, str paf, bool isLLog) {
+void readFile(ifstream& file, ReadOptions options, int history, bool isLLog) {
 
-    if (!file.is_open()) {
-        file.open(paf);
-        if (!fs::exists(paf)) {
-            prl("Error Opening file");
-            return;
-        }
-    }
+    if (!file.is_open())
+        prl("Fak can't open file... I suck! Trash me!");
 
     str line = "";
-    int counter = 0; 
+    int counter = 0;
 
     history = (bool)history * history + ((!(bool)history) * -1) + (((bool)history) * 1); // problem here
     // if history is 0, print all -> -1, else print the number of logs from first and add 1 to it
@@ -179,16 +174,16 @@ void readTrackerFile(ifstream& file, TrackerFileOptions tfo, int history) {
     int i = 0;
     bool isGenN = 0;
 
-    
+
     switch (tfo) {
     case G_IndexerAndData:
     case E_IndexerAndData:
+
         while (getline(file, line) || ((bool)++i))
             prl(i << '\t' << line);
         break;
     case LastLogs:
 
-        
         while (getline(file, line)) {
             if (line.find((char)1) != str::npos) {
                 isGenN = 1;
@@ -196,19 +191,19 @@ void readTrackerFile(ifstream& file, TrackerFileOptions tfo, int history) {
             }
             if (isGenN) {
                 if (line.find("Anime") == 0)
-                    readFile(file, Anime, 1, "", 1);
+                    readFile(file, Anime, 1, 1);
                 else if (line.find("Manga") == 0)
-                    readFile(file, Manga, 1, "", 1);
+                    readFile(file, Manga, 1, 1);
                 else if (line.find("Movies") == 0)
-                    readFile(file, Movies, 1, "", 1);
+                    readFile(file, Movies, 1, 1);
                 else
-                    readFile(file, Others, 1, "", 1);
+                    readFile(file, Others, 1, 1);
             }
             isGenN *= 0;
         }
         break;
     default:
-        prl("Wrong Choice! Damn, cmon edit the source code");
+        prl("\nWrong Choice! Damn, cmon edit the source code");
     }
 }
 
@@ -216,16 +211,74 @@ void readTrackerFile(ifstream& file, TrackerFileOptions tfo, int history) {
 void readTrackerFile(ifstream& file, bool& choice) {
     str validation;
     char c;
-    
+
     while (getline(file, validation)) {
         c = validation.at(0);
         if (validation == "")
             choice = 1;
-        else if ((c >= '0' || c <= '9') && validation.length() == 1)
+        else if ((c >= '0' || c <= '9') && validation.length() == 1) {
             choice = (c - '0');
+            return;
+        }
         else {
             prl("Error reading settings file");
             break;
         }
     }
+}
+
+
+
+
+// for others
+
+void writeFile(str paf, str& details, str& date, str& time, str name) {
+    str line = "", nameR = "";
+    
+    if (!fs::exists(paf)) {
+        ofstream file;
+        file.open(paf);
+        file << name << endl;
+        file << (char)1;
+        file.close();
+    }
+
+    ifstream fileIN(paf);
+    //ofstream fileOUT(paf);
+
+    getline(fileIN, nameR);
+    {
+        str temp = "";
+        while (getline(fileIN, temp)) {
+            line = line + temp + '\n';
+        }
+    }
+
+    ofstream fileOUT(paf);
+
+    fileOUT << nameR << endl;
+    fileOUT << (char)1 << endl;
+    fileOUT << date << endl << time << endl << details << endl;
+    fileOUT << line;
+
+    prl(line);
+}
+
+void writeFile(str paf, str& season, str& episode, str& details, str& date, str& time, str name)
+{
+}
+
+// only for settings file
+void writeFile(str paf, bool choice) {
+    ofstream file;
+    
+    if (!fs::exists(paf)) {
+        ofstream f;
+        f.open(paf);
+        f << 1;
+        f.close();
+    }
+    file.open(paf);
+    file << choice;
+    file.close();
 }
