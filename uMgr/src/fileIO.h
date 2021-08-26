85445ad6d01f1d91f31072012eb5203300a1e1b1
Someone <sangitabiswas0606@gmail.com>
2021-08-26 20:43:22 +0530

    Fns for Write file

--- uMgr/src/fileIO.h
+++ uMgr/src/fileIO.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <thread>
 
 using namespace std;
 
@@ -26,6 +27,11 @@ enum ReadOptions {
     Others
 };
 
+enum WriteOption
+{
+    Create, Add, NQuit, LogIt, NLogIt
+};
+
 enum TrackerFileOptions
 {
      LastLogs, G_IndexerAndData, E_IndexerAndData
@@ -35,32 +41,17 @@ enum TrackerFileOptions
 bool isspace(str& string1);
 
 
-
 // read from files
-//
 void readFile(ifstream& file, ReadOptions options, int history = 0, bool isLLog = 0);
 
-void readTrackerFile(ifstream& file, TrackerFileOptions tfo, int history = 0);
 // history shows no. of history for LastLogs.baka, if > 10 then it shutsdown silently
+void readTrackerFile(ifstream& file, TrackerFileOptions tfo, int history = 0);
 
 void readTrackerFile(ifstream& file, bool& choice); // only for settings.baka
 
 
 
 // write to files
-
-void splitFile(str& paf, str& nameR, str& leftOver, str& name); // split the name and log
-
-void writeFile(str paf, str& details, str& date, str& time, str name = "");
-// write file for others
-
-void writeFile(str paf, str& season, str& episode, str& details, str& date, str& time, str name = "");
-// write file for Anime!!!
-
-void writeFile(str paf, str& chapter, str& page, str& details, str& date, str& time, bool shit /* useless param for distinguishing */, str name = "");
-// write file for Manga!!!
-
-void writeFile(str paf, str& part, str& details, str& date, str& time, str name = "");
-// write file for movies
-
-void writeFile(str paf, bool choice);
+void writeFile(str paf, str& data, str genre = "Anime", int option = Create, str name = "");
+// write to setting file
+void writeFile(bool choice, str paf = (folderN + fsep + "Settings.baka"));
