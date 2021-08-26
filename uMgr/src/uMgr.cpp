ef10a8412667708e9eec5c0a9fede6a00c1647e8
Someone <sangitabiswas0606@gmail.com>
2021-08-26 20:45:44 +0530

    Write file fns Implemented the write file fns. Works fine till now...

--- uMgr/src/uMgr.cpp
+++ uMgr/src/uMgr.cpp
@@ -16,7 +16,16 @@ int main() {
             if (!fs::exists(pa)) {
                 ofstream file;
                 file.open(pa);
-                file << (char)1 << endl;
+                // file << (char)1;
+                file.close();
+            }
+        }
+        {
+            str pa = folderN + fsep + "AllLogs.hentai"; // dafuq 😂🤣😅
+            if (!fs::exists(pa)) {
+                ofstream file;
+                file.open(pa);
+                // file << (char)1;
                 file.close();
             }
         }
@@ -32,11 +41,15 @@ int main() {
                 fs::create_directory((pa + "Anime"));
                 fs::create_directory((pa + "Manga"));
                 fs::create_directory((pa + "Movies"));
+                fs::create_directory((pa + "BTS"));       // BTS is one of the best... fu if u don't like it, idc
+                fs::create_directory((pa + "BLACKPINK")); // hmm... I like BLACKPINK but not as much as BTS. Also fu if u don't like it, I still don't care
             }
         }
     }
-    
+
     str details = "subarashiiine", date = "58th August, 696969", time = "69:69:69 a.p.m";
-    writeFile(folderN + fsep + "smth.txt", details, date, time, "Anime");
+    str paf = folderN + fsep + "smth.txt", data = date + '\n' + time + '\n' + "2" + '\n' + "2" + '\n' + details;
+
+    writeFile(paf, data, "Anime", Create | Add, "Date A Live");
     return 0;
 }
